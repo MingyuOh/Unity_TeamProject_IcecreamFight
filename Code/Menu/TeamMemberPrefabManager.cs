253939066a2d49819e9d23862faad14c1b7a6449
MingyuOh <hotalsrb12@naver.com>
2023-02-16 18:57:16 +0900

    Change project

--- Code/Menu/TeamMemberPrefabManager.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TeamMemberPrefabManager : MonoSingleton<TeamMemberPrefabManager>
-{
-    public MenuTeamMemberPrefab[] menuTMprefabs;
-}
